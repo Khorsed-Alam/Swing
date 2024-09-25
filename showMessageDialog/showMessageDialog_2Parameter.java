5d1cc7b6ba00935a872f6b5a01b30708174042bf
Khorsed Alam <khorsedalam0472@gmail.com>
2024-09-25 23:36:39 +0600

    showMessageParameter_2Parameter

--- showMessageDialog/showMessageDialog_2Parameter.java
+++ showMessageDialog/showMessageDialog_2Parameter.java
@@ -2,7 +2,7 @@ package showMessageDialog;
 
 import javax.swing.*;
 
-public class MessageDialog1 {
+public class showMessageDialog_2Parameter {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null , "Wrong Password");
         //JOptionPane.showMessageDialog(Window Location null means "Center " , Message "Wrong Password");
