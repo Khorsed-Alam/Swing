118565046a9c9e9a0d4e8386e07cddfb0a2853ad
Khorsed Alam <khorsedalam0472@gmail.com>
2024-09-25 23:30:35 +0600

    showMessageDialog 2 Parameter

---
+++ showMessageDialog/MessageDialog1.java
@@ -0,0 +1,10 @@
+package showMessageDialog;
+
+import javax.swing.*;
+
+public class MessageDialog1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null , "Wrong Password");
+        //JOptionPane.showMessageDialog(Window Location null means "Center " , Message "Wrong Password");
+    }
+}
